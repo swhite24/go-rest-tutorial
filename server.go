297e4630fd2c7acff064f2692ebe0eb58a23d066
Steven White <steven.white@ensequence.com>
2014-12-21 14:01:16 -0500

    Add POST, DELETE

--- server.go
+++ server.go
@@ -34,6 +34,30 @@ func main() {
 		fmt.Fprintf(w, "%s", uj)
 	})
 
+	r.POST("/user", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		// Stub an user to be populated from the body
+		u := models.User{}
+
+		// Populate the user data
+		json.NewDecoder(r.Body).Decode(&u)
+
+		// Add an Id
+		u.Id = "foo"
+
+		// Marshal provided interface into JSON structure
+		uj, _ := json.Marshal(u)
+
+		// Write content-type, statuscode, payload
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(201)
+		fmt.Fprintf(w, "%s", uj)
+	})
+
+	r.DELETE("/user/:id", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		// TODO: only write status for now
+		w.WriteHeader(200)
+	})
+
 	// Fire up the server
 	http.ListenAndServe("localhost:3000", r)
 }
